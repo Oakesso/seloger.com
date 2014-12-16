e9e5bc38866a3b06d946a5a5aae44a42e3e5f7e8
Micky_Mic <mic100@hotmail.fr>
2014-12-16 03:03:46 +0100

    Update phase.py

--- phase.py
+++ phase.py
@@ -5,35 +5,28 @@ from tool_kit import url_todo
 
 def phase2(path_log) :
     try :
-        function_2()
+        function_2(path_log)
     except :
-        print "#------------------------------------#"
         print "phase 2 : 1st time function_1 failed to launch, re start of function"
-        print "\n"
         try :
-            function_2()
+            function_2(path_log)
         except :
-            print "#------------------------------------#"
             print "phase 2 : 2nd time function_1 failed to launch, re start of function"
-            print "\n"
-            function_2()
+            function_2(path_log)
 
 def phase3(path_log) :
-    function_3()
+    function_3(path_log)
 
 def phase4(path_log) :
     try :
-        function_4()
+        function_4(path_log)
     except :
-        print "#------------------------------------#"
         print "phase 4 : 1st time function_1 failed to launch, re start of function"
         print "\n"
         url_todo(path_log)
         try :
-            function_4()
+            function_4(path_log)
         except :
-            print "#------------------------------------#"
             print "phase 4 : 2nd time function_1 failed to launch, re start of function"
-            print "\n"
             url_todo(path_log)
-            function_4()
+            function_4(path_log)
